bbf438f743a91d55c30256c02cc1464884a1c64a
Oliver Sturm <oliver@oliversturm.com>
2020-10-06 13:34:35 +0100

    intro step1 starting point

--- resolve-intro-step1/config.app.js
+++ resolve-intro-step1/config.app.js
@@ -1,29 +1,30 @@
 const appConfig = {
   aggregates: [
     {
-      name: 'aggregate-name',
-      commands: 'common/aggregates/aggregate-name.commands.js',
-      projection: 'common/aggregates/aggregate-name.projection.js',
+      name: 'product',
+      commands: 'common/aggregates/product.commands.js',
+      projection: 'common/aggregates/product.projection.js',
     },
   ],
   readModels: [
     {
-      name: 'read-model-name',
-      projection: 'common/read-models/read-model-name.projection.js',
-      resolvers: 'common/read-models/read-model-name.resolvers.js',
+      name: 'products',
+      projection: 'common/read-models/products.projection.js',
+      resolvers: 'common/read-models/products.resolvers.js',
       connectorName: 'default',
     },
   ],
-  viewModels: [
-    {
-      name: 'view-model-name',
-      projection: 'common/view-models/view-model-name.projection.js',
-      serializeState: 'common/view-models/view-model-name.serialize_state.js',
-      deserializeState:
-        'common/view-models/view-model-name.deserialize_state.js',
-    },
-  ],
-  clientEntries: ['client/index.js'],
-}
 
-export default appConfig
+  // viewModels: [
+  //   {
+  //     name: 'view-model-name',
+  //     projection: 'common/view-models/view-model-name.projection.js',
+  //     serializeState: 'common/view-models/view-model-name.serialize_state.js',
+  //     deserializeState:
+  //       'common/view-models/view-model-name.deserialize_state.js',
+  //   },
+  // ],
+  clientEntries: ['client/index.js'],
+};
+
+export default appConfig;
